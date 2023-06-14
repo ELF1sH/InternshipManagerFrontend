940d3d2b2d0f176fa265352352358adb182d746a
Maksim Neverovskiy <max911z@icloud.com>
2023-06-15 02:01:37 +0700

    Resolve threads

--- src/pages/preferences/PreferencesPageView.tsx
+++ src/pages/preferences/PreferencesPageView.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-import { Button } from 'antd';
 
 import PageHeader from 'components/ui/molecules/pageHeader/PageHeader';
 import PlusIcon from 'components/ui/atoms/icons/PlusIcon';
 import { useModalViewModel } from 'components/ui/organisms/modal/context/ModalProvider';
+import Button from 'components/ui/atoms/button/Button';
+import VacanciesList from 'components/ui/organisms/vacanciesList/VacanciesList';
 
 import { usePreferencesPageViewModel } from 'pages/preferences/viewModel/context';
 import DnDWrapper from 'pages/preferences/components/dndWrapper/DnDWrapper';
-import VacanciesPageProvider from 'pages/vacancies/VacanciesPageProvider';
 
 const PreferencesPageView: React.FC = () => {
   const { wasEdited } = usePreferencesPageViewModel();
@@ -17,29 +17,24 @@ const PreferencesPageView: React.FC = () => {
   return (
     <>
       <PageHeader
-        header={(
-          <div style={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
-            Список предпочтений
-            <Button
-              type="text"
-              icon={<PlusIcon size={24} />}
-              style={{ display: 'flex', alignItems: 'center', gap: '4px' }}
-              onClick={() => openModal(
-                {
-                  formTitle: 'Добавление вакансии в список предпочтений',
-                  content: <VacanciesPageProvider />,
-                  style: { minWidth: '1000px' },
-                },
-              )}
-            >
-              Добавить
-
-            </Button>
-          </div>
-)}
+        header=" Список предпочтений"
         editable
         wasEdited={wasEdited}
-      />
+      >
+        <Button
+          type="text"
+          icon={<PlusIcon size={24} />}
+          onClick={() => openModal(
+            {
+              formTitle: 'Добавление вакансии в список предпочтений',
+              content: <VacanciesList companiesWithVacancies={[]} />,
+              style: { minWidth: '1000px' },
+            },
+          )}
+        >
+          Добавить
+        </Button>
+      </PageHeader>
       <DnDWrapper />
     </>
   );
