e59fe41598215a915b4eb6afea3472de12657288
Maksim Neverovskiy <max911z@icloud.com>
2023-06-15 04:17:43 +0700

    Add modal for create/edit template

--- src/components/ui/molecules/reportTemplate/ReportTemplate.tsx
+++ src/components/ui/molecules/reportTemplate/ReportTemplate.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card } from 'antd';
-import { DownloadOutlined, EyeOutlined } from '@ant-design/icons';
+import { DownloadOutlined, EditOutlined, EyeOutlined } from '@ant-design/icons';
 
 import Text from 'components/ui/atoms/text/Text';
 
@@ -8,17 +8,25 @@ interface ReportTemplateProps {
   title: string;
   turnInDate?: string;
   description?: string;
+  clickEditHandler?: (reportTemplate: ReportTemplateProps) => void
 }
 
 const ReportTemplate: React.FC<ReportTemplateProps> = ({
   title,
   turnInDate,
   description,
+  clickEditHandler,
 }) => (
   <Card
     title={title}
     actions={[
       <DownloadOutlined key="download" />,
+      <EditOutlined
+        onClick={() => {
+          clickEditHandler?.({ title, description });
+        }}
+        key="edit"
+      />,
       <EyeOutlined key="inspect" />,
     ]}
     style={{ maxWidth: '280px' }}
--- src/pages/templates/TemplatesPageView.tsx
+++ src/pages/templates/TemplatesPageView.tsx
@@ -3,17 +3,67 @@ import React from 'react';
 import Space from 'components/ui/atoms/space/Space';
 import ReportTemplate from 'components/ui/molecules/reportTemplate/ReportTemplate';
 import PageHeader from 'components/ui/molecules/pageHeader/PageHeader';
+import Button from 'components/ui/atoms/button/Button';
+import PlusIcon from 'components/ui/atoms/icons/PlusIcon';
+import { useModalViewModel } from 'components/ui/organisms/modal/context/ModalProvider';
 
-const TemplatesPageView: React.FC = () => (
-  <>
-    <PageHeader header="Шаблоны дневника практики" />
-    <Space $wrap gap={20}>
-      <ReportTemplate title="Дневник практики 1" description="Тут идет описание дневника практики номер 1" />
-      <ReportTemplate title="Дневник практики 1" description="Тут идет описание дневника практики номер 1" />
-      <ReportTemplate title="Дневник практики 1" description="Тут идет описание дневника практики номер 1" />
-      <ReportTemplate title="Дневник практики 1" description="Тут идет описание дневника практики номер 1" />
-    </Space>
-  </>
-);
+import { TemplateForm } from 'pages/templates/components/TemplateForm';
+
+const TemplatesPageView: React.FC = () => {
+  const { openModal } = useModalViewModel();
+  return (
+    <>
+      <PageHeader header="Шаблоны дневника практики">
+        <Button
+          type="text"
+          icon={<PlusIcon size={24} />}
+          onClick={() => {
+            openModal({
+              formTitle: 'Создание шаблона',
+              content: <TemplateForm />,
+            });
+          }}
+        >
+          Создать шаблон
+
+        </Button>
+      </PageHeader>
+      <Space $wrap gap={20}>
+        <ReportTemplate
+          title="Дневник практики 1"
+          description="Тут идет описание дневника практики номер 1"
+          clickEditHandler={(template) => openModal({
+            formTitle: 'Редактирование шаблона',
+            content: <TemplateForm defaultValues={template} />,
+          })}
+        />
+        <ReportTemplate
+          title="Дневник практики 1"
+          description="Тут идет описание дневника практики номер 1"
+          clickEditHandler={(template) => openModal({
+            formTitle: 'Редактирование шаблона',
+            content: <TemplateForm defaultValues={template} />,
+          })}
+        />
+        <ReportTemplate
+          title="Дневник практики 1"
+          description="Тут идет описание дневника практики номер 1"
+          clickEditHandler={(template) => openModal({
+            formTitle: 'Редактирование шаблона',
+            content: <TemplateForm defaultValues={template} />,
+          })}
+        />
+        <ReportTemplate
+          title="Дневник практики 1"
+          description="Тут идет описание дневника практики номер 1"
+          clickEditHandler={(template) => openModal({
+            formTitle: 'Редактирование шаблона',
+            content: <TemplateForm defaultValues={template} />,
+          })}
+        />
+      </Space>
+    </>
+  );
+};
 
 export default TemplatesPageView;
